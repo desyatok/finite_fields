a76859a801e0d049930afab62bcd7b7b7f10852f
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-03-12 23:25:15 +0300

    Implement get constants functions

--- library.c
+++ library.c
@@ -14,3 +14,30 @@ Field* getField(uint8_t mod, const uint8_t *poly, uint8_t poly_deg)
     }
     return newField;
 }
+
+FieldMember *getZero(Field *field)
+{
+    FieldMember *zero = (FieldMember *)malloc(sizeof(FieldMember));
+    zero->field = field;
+    zero->deg = field->poly_deg;
+    zero->poly = (uint8_t *)malloc(zero->deg * sizeof(uint8_t));
+    for (uint8_t i = 0; i < zero->deg; ++i)
+    {
+        zero->poly[i] = 0;
+    }
+    return zero;
+}
+
+FieldMember *getIdentity(Field *field)
+{
+    FieldMember *identity = (FieldMember *)malloc(sizeof(FieldMember));
+    identity->field = field;
+    identity->deg = field->poly_deg;
+    identity->poly = (uint8_t *)malloc(identity->deg * sizeof(uint8_t));
+    for (uint8_t i = 0; i < identity->deg - 1; ++i)
+    {
+        identity->poly[i] = 0;
+    }
+    identity->poly[identity->deg - 1] = 1;
+    return identity;
+}
