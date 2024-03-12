130cfe18292870ba70638cdc8d1fa291debe6710
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-03-12 23:14:27 +0300

    Implement getField function
    It initializes a new finite field with mod^poly_deg elements.

--- library.c
+++ library.c
@@ -1,7 +1,16 @@
+#include <stdlib.h>
+
 #include "library.h"
 
-#include <stdio.h>
-
-void hello(void) {
-    printf("Hello, World!\n");
+Field* getField(uint8_t mod, const uint8_t *poly, uint8_t poly_deg)
+{
+    Field *newField = (Field *)malloc(sizeof(Field));
+    newField->mod = mod;
+    newField->poly_deg = poly_deg;
+    newField->irred_poly = (uint8_t *)malloc(poly_deg * sizeof(uint8_t));
+    for (size_t i = 0; i < poly_deg; ++i)
+    {
+        newField->irred_poly[i] = poly[i];
+    }
+    return newField;
 }
