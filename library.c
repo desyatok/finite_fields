d01e90c9f05ba35386f08275e7b029a50e5cc781
Pavel Desyatnikov <desyatnikovpvl@gmail.com>
2024-03-13 22:09:14 +0300

    Refactor deg back to dim
    More convenient to use dimensions, not degrees

--- library.c
+++ library.c
@@ -2,13 +2,13 @@
 
 #include "library.h"
 
-Field *getField(uint8_t mod, const uint8_t *poly, uint8_t poly_deg)
+Field *getField(uint8_t mod, const uint8_t *poly, uint8_t poly_dim)
 {
     Field *newField = (Field *)malloc(sizeof(Field));
     newField->mod = mod;
-    newField->poly_deg = poly_deg;
-    newField->irred_poly = (uint8_t *)malloc((poly_deg + 1) * sizeof(uint8_t));
-    for (size_t i = 0; i <= poly_deg; ++i)
+    newField->poly_dim = poly_dim;
+    newField->irred_poly = (uint8_t *)malloc(poly_dim * sizeof(uint8_t));
+    for (size_t i = 0; i < poly_dim; ++i)
     {
         newField->irred_poly[i] = poly[i];
     }
@@ -19,9 +19,9 @@ FieldMember *getZero(Field *field)
 {
     FieldMember *zero = (FieldMember *)malloc(sizeof(FieldMember));
     zero->field = field;
-    zero->deg = field->poly_deg;
-    zero->poly = (uint8_t *)malloc((zero->deg + 1) * sizeof(uint8_t));
-    for (uint8_t i = 0; i <= zero->deg; ++i)
+    zero->dim = field->poly_dim;
+    zero->poly = (uint8_t *)malloc(zero->dim * sizeof(uint8_t));
+    for (uint8_t i = 0; i < zero->dim; ++i)
     {
         zero->poly[i] = 0;
     }
@@ -32,12 +32,12 @@ FieldMember *getIdentity(Field *field)
 {
     FieldMember *identity = (FieldMember *)malloc(sizeof(FieldMember));
     identity->field = field;
-    identity->deg = field->poly_deg;
-    identity->poly = (uint8_t *)malloc((identity->deg + 1) * sizeof(uint8_t));
-    for (uint8_t i = 0; i < identity->deg; ++i)
+    identity->dim = field->poly_dim;
+    identity->poly = (uint8_t *)malloc(identity->dim * sizeof(uint8_t));
+    for (uint8_t i = 0; i < identity->dim - 1; ++i)
     {
         identity->poly[i] = 0;
     }
-    identity->poly[identity->deg] = 1;
+    identity->poly[identity->dim - 1] = 1;
     return identity;
 }
